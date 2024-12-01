f75a3ba957b469c4f5ad048111b13852cc0a6581
Matouš Skála <skala.matous@gmail.com>
2024-12-01 19:54:40 +0100

    Add day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,39 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableListOf<Int>()
+        input.forEach { line ->
+            val numbers = line.split("   ")
+            left.add(numbers[0].toInt())
+            right.add(numbers[1].toInt())
+        }
+        left.sort()
+        right.sort()
+        return left.withIndex().sumOf { (index, value) ->
+            (left[index] - right[index]).absoluteValue
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val left = mutableListOf<Int>()
+        val right = mutableMapOf<Int, Int>()
+        input.forEach { line ->
+            val numbers = line.split("   ")
+            left.add(numbers[0].toInt())
+            right[numbers[1].toInt()] = right.getOrDefault(numbers[1].toInt(), 0) + 1
+        }
+        return left.sumOf {
+            it * (right[it] ?: 0)
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val output = part1(testInput)
+    output.println()
+    check(output == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
